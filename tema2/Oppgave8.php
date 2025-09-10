b56a80f57b741fa6706f906104da74aebfb586fa
ergas05 <ergas6012@usn.no>
2025-09-10 13:14:35 +0200

    khv

--- tema2/Oppgave8.php
+++ tema2/Oppgave8.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Oppgave 8</title>
+</head>
+<body>
+<h3>Oppgave 8</h3>
+<form method="post" action="" id="Oppgave8" name="oppgave8">
+Hva er 3 ganger 3? <input type="text" id="svar" name="svar" required /> <br />
+<input type="submit" value="Fortsett" id="fortsett" name="fortsett" />
+<input type="reset" value="Nullstill" name="nullstill" id="nullstill" /> <br />
+</form>
+</body>
+</html> 
+
 <?php /* Oppgave 1 */
 /*
 /* Programmet mottar fra et HTML-skjema et svar på spørsmålet "Hva er 3 ganger 3 ?"
