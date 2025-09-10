1baea7f3651a5c7e7b8199edc39ecc5d5a659855
ergas05 <ergas6012@usn.no>
2025-09-10 12:13:00 +0200

    hei

--- tema2/Oppgave4.php
+++ tema2/Oppgave4.php
@@ -4,8 +4,8 @@
 /* "Er du gift (j/n) ?" og "Har du barn (j/n) ?"
 /* Programmet sjekker om det er svart j eller n på spørsmålet og skriver ut en passende melding
 */
-$gift=$_POST ["gift"];
-$barn=$_POST ["barn"];
+$gift=$_POST ["gift"];  
+$barn=$_POST ["barn"];  
 if (!$gift || !$barn) /* det er ikke svart på begge spørsmålene */
 {
 print("Du har ikke svart p&aring; begge sp&oslash;rsm&aring;lene <br/>");
